3bdc6eb88ec709c8b22866b05f68ba84ff1017c5
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-05-26 20:58:17 +0500

    Add MainViewModel

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -3,8 +3,9 @@ package com.example.movies;
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&page=2&limit=5")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=5")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 }
