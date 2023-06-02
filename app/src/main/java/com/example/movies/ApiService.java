0252f7bbe03211768912d551b9674b01766bd2ca
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-06-03 00:08:17 +0500

    Загрузка трейлеров

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -9,4 +9,7 @@ public interface ApiService {
     @GET("movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&field=rating.kp&search=4-10&sortField=votes" +
             ".kp&sortType=-1&limit=30")
     Single<MovieResponse> loadMovies(@Query("page") int page);
+
+    @GET("movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&field=id")
+    Single<TrailerResponse> loadResponse(@Query("search") int id);
 }
