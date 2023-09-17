06b5546e0f5fa31dadf0b179ece58617da9766a4
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-09-17 12:11:24 +0500

    Оператор map

--- app/src/main/java/com/example/movies/MovieDetailViewModel.java
+++ app/src/main/java/com/example/movies/MovieDetailViewModel.java
@@ -14,6 +14,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.schedulers.TestScheduler;
 
@@ -40,15 +41,16 @@ public class MovieDetailViewModel extends AndroidViewModel {
         Disposable disposable = ApiFactory.apiService.loadTrailers(id)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<TrailerResponse>() {
+                .map(new Function<TrailerResponse, List<Trailer>>() {
                     @Override
-                    public void accept(TrailerResponse trailerResponse) throws Throwable {
-                        trailers.setValue(trailerResponse.getVideos().getTrailerList().getTrailers());
+                    public List<Trailer> apply(TrailerResponse trailerResponse) throws Throwable {
+                        return trailerResponse.getVideos().getTrailerList().getTrailers();
                     }
-                }, new Consumer<Throwable>() {
+                })
+                .subscribe(new Consumer<List<Trailer>>() {
                     @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.d(TAG, throwable.toString());
+                    public void accept(List<Trailer> trailerList) throws Throwable {
+                        trailers.setValue(trailerList);
                     }
                 });
         compositeDisposable.add(disposable);
