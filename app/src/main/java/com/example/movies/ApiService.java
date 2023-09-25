10d76039426afc412da0df12d5a000e5d276c7e5
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-09-25 19:04:59 +0500

    Загрузка отзывов + оператор map!

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -6,10 +6,13 @@ import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&field=rating.kp&search=4-10&sortField=votes" +
+    @GET("v1.3/movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&field=rating.kp&search=4-10&sortField=votes" +
             ".kp&sortType=-1&limit=30")
     Single<MovieResponse> loadMovies(@Query("page") int page);
 
-    @GET("movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&selectFields=videos&sortField=videos.trailers.url&field=id")
+    @GET("v1.3/movie?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&selectFields=videos&sortField=videos.trailers.url&field=id")
     Single<TrailerResponse> loadTrailers(@Query("search") int id);
+
+    @GET("v1/review?token=6H5E8VR-C8AME3F-NT73Q1F-GRX4DZV&field=movieId")
+    Single<ReviewResponse> loadReviews(@Query("search") int id);
 }
---
+++ app/src/main/java/com/example/movies/ReviewsAdapter.java
@@ -0,0 +1,18 @@
+package com.example.movies;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ReviewsAdapter {
+
+
+    static class ReviewViewHolder extends RecyclerView.ViewHolder {
+
+        public ReviewViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
